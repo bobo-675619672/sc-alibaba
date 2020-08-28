3e93c3dc8566aa7f9640089276530bf8c2db5d44
菠萝油头00 <675619672@qq.com>
2020-08-28 16:20:53 +0800

    新增项目代码

---
+++ gateway/src/test/java/com/dw/sc/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw.sc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
