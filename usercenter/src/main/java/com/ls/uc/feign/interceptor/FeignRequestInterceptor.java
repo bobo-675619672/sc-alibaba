bc17b830c2a4b8aeacd7cc253c7f0255f86c226c
菠萝油头00 <675619672@qq.com>
2020-09-11 14:54:10 +0800

    【测试】更新代码

--- usercenter/src/main/java/com/ls/uc/feign/interceptor/FeignRequestInterceptor.java
+++ usercenter/src/main/java/com/ls/uc/feign/interceptor/FeignRequestInterceptor.java
@@ -3,6 +3,7 @@ package com.ls.uc.feign.interceptor;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 
 /**
  * feign请求拦截器
@@ -14,6 +15,12 @@ public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
         log.info("所有feign请求都经过这里...");
+        // 打印相关信息
+        String method = template.method();
+        String url = template.url();
+        String variables = String.join(",", template.variables());
+
+        log.info("method:{}, url:{}, variables:{}", method, url, variables);
     }
 
 }
