f7874eeea7d4462d49dfb1b85aae634c9ef18221
菠萝油头00 <675619672@qq.com>
2020-09-11 14:01:27 +0800

    【测试】更新代码

--- usercenter/src/main/java/com/ls/uc/config/SwaggerConfig.java
+++ usercenter/src/main/java/com/ls/uc/config/SwaggerConfig.java
@@ -45,7 +45,7 @@ public class SwaggerConfig {
                 .pathProvider(new RelativePathProvider(servletContext) {
                     @Override
                     public String getApplicationBasePath() {
-                        return "/uc";
+                        return "/usercenter";
                     }
                 })
                 .select()
