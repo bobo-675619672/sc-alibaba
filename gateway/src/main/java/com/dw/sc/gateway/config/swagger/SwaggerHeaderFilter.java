bd6687e41f34d9113ea4433de13356815c214e4b
菠萝油头00 <675619672@qq.com>
2020-09-18 10:17:54 +0800

    【测试】更新代码

--- gateway/src/main/java/com/dw/sc/gateway/config/swagger/SwaggerHeaderFilter.java
+++ gateway/src/main/java/com/dw/sc/gateway/config/swagger/SwaggerHeaderFilter.java
@@ -1,5 +1,6 @@
 package com.dw.sc.gateway.config.swagger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 @Component
+@Slf4j
 public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
     private static final String HEADER_NAME = "X-Forwarded-Prefix";
     private static final String URI = "/v2/api-docs";
@@ -16,10 +18,12 @@ public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             String path = request.getURI().getPath();
-            if (!StringUtils.endsWithIgnoreCase(path,URI)) {
-                return chain.filter(exchange);
-            }
+            log.info("path:{}", path);
+//            if (!StringUtils.endsWithIgnoreCase(path,URI)) {
+//                return chain.filter(exchange);
+//            }
             String basePath = path.substring(0, path.lastIndexOf(URI));
+            log.info("basePath:{}", basePath);
             ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
             ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
             return chain.filter(newExchange);
